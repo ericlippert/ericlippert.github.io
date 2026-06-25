c4ccc9e30cbbd14569fe5734de202a958a80b9df
jjbird0 <james.bird011@gmail.com>
2026-06-25 01:55:26 -0700

    fix issue #1
    fix issue #1

---
+++ js.js
@@ -0,0 +1,16 @@
+const inputbox = document.getElementById('inputbox');
+const submitbutton = document.getElementById('submitbutton');
+const outputtext = document.getElementById('outputtext');
+const enteryourname = document.getElementById('enteryourname');
+
+// let instead of var but let me know if var is better for this -jj
+let storedData = "";
+
+submitbutton.addEventListener('click', () => {
+    storedData = inputbox.value;
+    inputbox.style.display = 'none';
+    enteryourname.style.display = 'none';
+    submitbutton.style.display = 'none';
+    console.log(storedData);
+    outputtext.textContent = "hello " + storedData + ", welcome to the dungeon.";
+});
